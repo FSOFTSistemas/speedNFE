8448d61fe62f102451f9c0f63dec3ad6d47ba114
Rodrigo Leandro <rodrigo.leandro.zx@gmail.com>
2023-12-04 17:38:38 -0300

    Atualizações

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-5v3YT0auvSjDz7JeF5VpJ0wWJ1R3r1Pj+Za5r+yF3e8sd9pWcTo1g2Bd25gT5toL50Rlh24RZjLOt4L/2ecClcA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{ asset('css/homePage.css') }}">
     <title>speedNFE</title>
 </head>
@@ -18,6 +19,7 @@
                 <a href="{{ route('homePage') }}">Home</a>
                 <a href="{{ route('Planos') }}">Planos</a>
                 <a href="{{ route('login') }}">Login</a>
+                
             </nav>
         </header>
     </div>
@@ -25,13 +27,53 @@
     <div class="background">
         <div class="container-2" id="text-home">
             <div class="text-left">
-                <img src="{{ asset("css/Imagem-nota-fiscal.png") }}" alt="Your Image">
+                <img id="laptop" src="{{ asset("css/Nota-fiscal-eletronica-Saiba-como-emitir-removebg-preview.png") }}">
             </div>
             <div class="text-right">
-                <h1>Emissor de Nota <br>Fiscal Eletrônica <br>e WEB DANFE Online</h1>
+                <h1>Emissor de Nota Fiscal Eletrônica e WEB DANFE Online</h1>
+                <h4>Conheça os benefícios de ter um <br>Certificado Digital</h4>
+                <a href="{{ route('Planos') }}"><button class="verPlanos" > Clique Aqui</button></a>
             </div>
         </div>
     </div>
 
+
+    <div class="background">
+        <div class="container" id="card-plano">
+            <div class="panel pricing-table">
+            
+                
+                <div class="pricing-plan">
+                    <h2 class="pricing-header"> Emissão de Notas Gratuitas</h2>
+                    <ul class="pricing-features">
+                        <li class="pricing-features-item"><i class="fa fa-check"></i> Todo mês, até 15 notas sem custo algum</li>
+                        <li class="pricing-features-item"><i class="fas fa-check"></i> Atualizações e melhorias constantes sem custo adicional</li>
+                    </ul>
+                </div>
+                
+                <div class="pricing-plan">
+                    <h2 class="pricing-header">Plano Profissional</h2>
+                    <ul class="pricing-features">
+                        <li class="pricing-features-item"><i class="fas fa-check"></i> Envio ilimitado do seu plano profissional</li>
+                        <li class="pricing-features-item"><i class="fas fa-check"></i> Envio de e-mail com o arquivo XML e DANFE</li>
+                        <li class="pricing-features-item"><i class="fas fa-check"></i> Emissão a partir de qualquer computador, sem precisar baixar nada.</li>
+                    </ul>
+                </div>
+                
+                <div class="pricing-plan">
+                    <h2 class="pricing-header">Comprometimento e Segurança</h2>
+                    <ul class="pricing-features">
+                        <li class="pricing-features-item"><i class="fas fa-check"></i> Armazenamento dos arquivos em segurança</li>
+                        <li class="pricing-features-item"><i class="fas fa-check"></i> Responsabilidade com a legislação</li>
+                        <li class="pricing-features-item"><i class="fas fa-check"></i> Cópias de segurança automáticas</li>
+                    </ul>
+                </div>
+                
+            
+            </div>
+        </div>
+    </div>
+    
+    
 </body>
 </html>
