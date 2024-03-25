7e8610f66318d690c4a0c878b42b48dcd78a034c
FSOFTSistemas <fsoftsistemas@gmail.com>
2024-03-25 09:42:42 -0300

    style: Ajustar placeholder dos campos de login

--- resources/views/vendor/adminlte/auth/login.blade.php
+++ resources/views/vendor/adminlte/auth/login.blade.php
@@ -46,11 +46,11 @@
                 <form action="{{ $login_url }}" method="post">
                     @csrf
 
-                    
+
                     <div class="input-group mb-3">
                         <p><label for="email" class="form-label" id="email_label">Email:</label></p><br><br>
                         <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
-                            value="{{ old('email') }}" placeholder="{{ __('adminlte::adminlte.email') }}" autofocus>
+                            value="{{ old('email') }}" placeholder="email" autofocus>
 
                         <div class="input-group-append">
                             <div class="input-group-text">
@@ -69,7 +69,7 @@
                         <p><label for="password" class="form-label" id="senha_label">Senha:</label></p><br><br>
                         <input type="password" name="password"
                             class="form-control @error('password') is-invalid @enderror"
-                            placeholder="{{ __('adminlte::adminlte.password') }}">
+                            placeholder="senha">
 
                         <div class="input-group-append">
                             <div class="input-group-text">
