f292c318092d1d6eb09b0fda2960891b130e854a
Rodrigo Leandro <rodrigo.leandro.zx@gmail.com>
2024-03-26 18:06:04 -0300

    Incio da nova tela de vendas

--- resources/views/homePage2.blade.php
+++ resources/views/homePage2.blade.php
@@ -6,82 +6,89 @@
   <title>Header Responsivo</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="{{ asset('css/Telas_Principais/homePage2.css') }}">
-  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
+<header class="navbar navbar-expand-lg navbar-light bg-transparent fixed-top ">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="{{ asset('css/logo.png') }}" class="img-fluid" alt="Logo"> <!-- Adicionando a classe "img-fluid" para tornar a imagem responsiva -->
+      </a>
 
-    <header class="navbar navbar-expand-lg navbar-light bg-transparent position-relative">
-        <div class="container">
-          <a class="navbar-brand" href="#">
-            <img src="{{ asset('css/logo.png') }}">
-          </a>
-      
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-      
-          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Services</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </header>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-<div class="container" id="container-2">
-  <div class="row mt-md-4" id="row-1"> 
-    <div class="col-md-6">
-      <img src="{{ asset('css/Nota-fiscal-eletronica-Saiba-como-emitir-removebg-preview.png') }}" class="img-fluid">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto"> 
+          <li class="nav-item">
+            <a class="nav-link home-link" href="{{ route('homePage2') }}">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+         
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="col-md-6">
-      <p class="text-large">Emissor de Nota Fiscal Eletrônica e WEB DANFE Online</p>
-      <p class="text-small">Conheça os benefícios de ter um Certificado Digital</p>
+</header>
+
+
+<div class="fullscreen-image">
+  <img src="{{ asset('css/imagem-fundo.jpg') }}" class="img-fluid" alt="Imagem de fundo"> <!-- Adicionando a classe "img-fluid" para tornar a imagem responsiva -->
+  <div class="text-overlay-left">
+    <h2>Emita Notas Fiscais Facilmente</h2>
+    <h5>Agilize suas operações comerciais em Garanhuns com um sistema rápido e seguro.</h5>
+    <button class="btn btn-primary btn-square">Saiba Mais</button> <!-- Adicionando um botão após os textos -->
+  </div>
+  <div class="text-overlay-right">
+    <h3>Conheça Nossos Serviços e Seja um de Nossos CLientes</h3>
+  </div>
+</div>
+
+<div class="container-2">
+  <h2 class="text-left">Serviços</h2>
+  <div class="row justify-content-around">
+    <div class="col-md-4">
+      <div class="card grande">
+        <div class="overlay"></div>
+        <img src="{{ asset('css/Mulher-no-Computador.jpg') }}" alt="Serviço 1" class="img-fluid">
+        <div class="card-text">
+          <h2>Qualquer coisa para exemplificar</h2>
+          <p>dajwnjkanwdjnawjkdnajkwndjanwdjknawjdn</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card grande">
+        <div class="overlay"></div>
+        <img src="{{ asset('css/Nota-fiscal-eletronica-Saiba-como-emitir-removebg-preview.png') }}" alt="Serviço 2" class="img-fluid">
+        <div class="card-text">
+          <h2>Qualquer coisa para exemplificar</h2>
+          <p>dajwnjkanwdjnawjkdnajkwndjanwdjknawjdn</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card grande">
+        <div class="overlay"></div>
+        <img src="{{ asset('css/Nota-fiscal-eletronica-Saiba-como-emitir-removebg-preview.png') }}" alt="Serviço 3" class="img-fluid">
+        <div class="card-text">
+          <h2>Qualquer coisa para exemplificar</h2>
+          <p>dajwnjkanwdjnawjkdnajkwndjanwdjknawjdn</p>
+        </div>
+      </div>
     </div>
   </div>
 </div>
 
 
-<div class="row"> 
-    <div class="container">
-
-        <h3> Planos Exclusivos, Com Muitas Vantagens</h3>
-        <h5> "Descubra nossos planos exclusivos repletos de benefícios feitos sob medida para você. Aproveite soluções inteligentes e economize tempo e dinheiro com nossas opções flexíveis. Escolha qualidade, escolha eficiência, escolha nossos planos hoje." </h5>
-        <div class="text-center">
-            <button type="button" class="button">Conheça os Planos</button>
-          </div>
-    </div>
-   
-    </div>
-
-    <div class="main-card">
-        <div class="title">Plano de Benefícios</div>
-        <div class="sub-card">
-          <div class="benefit-title">Benefício 1</div>
-          <div class="benefit-description">Descrição do Benefício 1.</div>
-        </div>
-        <div class="sub-card">
-          <div class="benefit-title">Benefício 2</div>
-          <div class="benefit-description">Descrição do Benefício 2.</div>
-        </div>
-        <div class="sub-card">
-          <div class="benefit-title">Benefício 3</div>
-          <div class="benefit-description">Descrição do Benefício 3.</div>
-        </div>
-      </div>
-
 <!-- Scripts -->
+<script src="homePage2.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
