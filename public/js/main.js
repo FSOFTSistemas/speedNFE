27c3081d9aac1b6f204c5c01290ff1075e8e4b12
FSOFTSistemas <fsoftsistemas@gmail.com>
2024-03-28 12:05:31 -0300

    Finalização do Site

--- public/js/main.js
+++ public/js/main.js
@@ -82,3 +82,16 @@
 		}
 
 })(jQuery);
+
+
+
+    function toggleMenu() {
+        var nav = document.getElementById('nav');
+        if (nav.classList.contains('open')) {
+            nav.classList.remove('open');
+        } else {
+            nav.classList.add('open');
+        }
+    }
+
+
--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -35,15 +35,18 @@
             <p>Simplifique a emissão de notas fiscais</p>
             <p>Emita notas fiscais de qualquer lugar, sem a necessidade de um computador</p>
             <ul class="actions special">
-                <li><a href="#" class="button">Saiba mais</a></li>
+                <li><a href="https://w.app/RSq5E3" target="_blank" class="button">Saiba mais</a></li>
+                <li><a href="{{Route('login')}}" target="_blank" class="button" style="margin-left: 40px;">Ja tem conta ? Faça Login</a></li>
+
             </ul>
+
         </section>
 
         <!-- Main -->
         <section id="main" class="container">
 
 
-			<section class="box special features">
+            <section class="box special features">
                 <h3 style="text-align: center;"> Serviços</h3>
                 <div class="features-row">
                     <section>
@@ -82,7 +85,7 @@
 
 
 
-			
+
             <section class="box special">
                 <header class="major">
                     <h2>Emita Notas Fiscais Facilmente</h2>
@@ -93,162 +96,161 @@
                 <span class="image featured"><img src="{{ asset('css/images/tela.png') }}" alt="" /></span>
             </section>
 
-		</section>
+        </section>
 
-            <div class="row">
-                <div class="col-4 col-12-narrower">
+        <div class="row" id="planos">
 
-                    <section class="box special">
-                        <span class="image featured"><img src="{{ asset('css/images/banner.jpg') }} " alt=""
-                                style="height: 400px;" /></span>
-                        <h3>Plano Básico</h3>
-                        <p>Emissao de 1 nota teste grátis<br>
-                            Cadastro de até 12 clientes<br>
-                            Emissão de até 10 notas p/mês<br>
-                            Cadastro de até 10 produtos<br>
-                            Emissão de NFe<br>
-                            Suporte via email
-                        </p>
-                        <ul class="actions special">
-                            <li><a href="#" class="button alt">Saiba mais</a></li>
-                        </ul>
-                    </section>
+                <h2>Conheça Nossos Planos</h2>
+                <div class="row" id="card-planos">
 
-                </div>
-                <div class="col-4 col-12-narrower">
+                    <div class="col-4 col-12-narrower">
 
-                    <section class="box special">
-                        <span class="image featured"><img src="{{ asset('css/images/conceito-de-relatorio-de-graficos-visuais-de-grafico-de-negocios.jpg') }}" alt=""
-                                style="height: 400px;" /></span>
-                        <h3>Plano Advanced</h3>
-                        <p>Emissao de 1 nota teste grátis<br>
-                            Cadastro de até 20 clientes<br>
-                            Emissão de até 20 notas p/mês<br>
-                            Cadastro de até 20 produtos<br>
-                            Emissão de NFe<br>
-                            Suporte via whatsApp<br>
-                        </p>
-                        <ul class="actions special">
-                            <li><a href="#" class="button alt">Saiba mais</a></li>
-                        </ul>
-                    </section>
+                        <section class="box special">
+                            <span class="image featured"><img src="{{ asset('css/images/banner.jpg') }} " alt="" style="height: 400px;" /></span>
+                            <h3>Plano Básico</h3>
+                            <p>Emissao de 1 nota teste grátis<br>
+                                Cadastro de até 12 clientes<br>
+                                Emissão de até 10 notas p/mês<br>
+                                Cadastro de até 10 produtos<br>
+                                Emissão de NFe<br>
+                                Suporte via email
+                            </p>
+                            <ul class="actions special">
+                                <li><a href="https://w.app/nWIw45" class="button alt">Saiba mais</a></li>
+                            </ul>
+                        </section>
 
-                </div>
-                <div class="col-4 col-12-narrower">
+                    </div>
+                    <div class="col-4 col-12-narrower">
 
-                    <section class="box special">
-                        <span class="image featured"><img src="{{ asset('css/images/trabalhadores-de-escritorio-usando-graficos-de-financas.jpg') }}" alt=""
-                                style="height: 400px;" /></span>
-                        <h3>Plano Premium</h3>
-                        <p>Emissao de 1 nota teste grátis<br>
-                            Cadastro ilimitado de clientes<br>
-                            Emissão de até 40 notas p/mês<br>
-                            Cadastro ilimitado de produtos<br>
-                            Emissao de NFe e MDFe<br>
-                            Suporte via whatsApp<br>
-                        </p>
-                        <ul class="actions special">
-                            <li><a href="#" class="button alt">Saiba mais</a></li>
-                        </ul>
-                    </section>
+                        <section class="box special">
+                            <span class="image featured"><img src="{{ asset('css/images/conceito-de-relatorio-de-graficos-visuais-de-grafico-de-negocios.jpg') }}" alt="" style="height: 400px;" /></span>
+                            <h3>Plano Advanced</h3>
+                            <p>Emissao de 1 nota teste grátis<br>
+                                Cadastro de até 20 clientes<br>
+                                Emissão de até 20 notas p/mês<br>
+                                Cadastro de até 20 produtos<br>
+                                Emissão de NFe<br>
+                                Suporte via whatsApp<br>
+                            </p>
+                            <ul class="actions special">
+                                <li><a href="https://w.app/XcUVa7" class="button alt">Saiba mais</a></li>
+                            </ul>
+                        </section>
 
+                    </div>
+                    <div class="col-4 col-12-narrower">
+
+                        <section class="box special">
+                            <span class="image featured"><img src="{{ asset('css/images/trabalhadores-de-escritorio-usando-graficos-de-financas.jpg') }}" alt="" style="height: 400px;" /></span>
+                            <h3>Plano Premium</h3>
+                            <p>Emissao de 1 nota teste grátis<br>
+                                Cadastro ilimitado de clientes<br>
+                                Emissão de até 40 notas p/mês<br>
+                                Cadastro ilimitado de produtos<br>
+                                Emissao de NFe e MDFe<br>
+                                Suporte via whatsApp<br>
+                            </p>
+                            <ul class="actions special">
+                                <li><a href=" https://w.app/6f4TY9" class="button alt">Saiba mais</a></li>
+                            </ul>
+                        </section>
+
+                    </div>
                 </div>
             </div>
 
-			
-			<section id="main" class="container" style="margin-top: 10px">
-            <section class="box special">
-                <header class="major">
-					<h2>Sobre Nós</h2>
-                    <h4>Emita Notas Fiscais Facilmente</h4>
-                    <p style="font-size: 18px">
-                        A SpeedNFe é uma plataforma inovadora que oferece uma solução completa para emissão e gestão de
-                        NFe
-                        e NFC-e. Somos uma empresa especializada em soluções fiscais, com mais de 10 anos de experiência
-                        no
-                        mercado, e estamos comprometidos em oferecer aos nossos clientes a melhor experiência possível.
-                    </p>
+            <section id="main" class="container" style="margin-top: 10px">
+                <section class="box special">
+                    <header class="major">
+                        <h2>Sobre Nós</h2>
+                        <h4>Emita Notas Fiscais Facilmente</h4>
+                        <p style="font-size: 18px">
+                            A SpeedNFe é uma plataforma inovadora que oferece uma solução completa para emissão e gestão de
+                            NFe
+                            e NFC-e. Somos uma empresa especializada em soluções fiscais, com mais de 10 anos de experiência
+                            no
+                            mercado, e estamos comprometidos em oferecer aos nossos clientes a melhor experiência possível.
+                        </p>
 
-					
-                    <h3 style="margin-top: 25px">Nossos Diferenciais</h3>
-				
-                    <ul style="border-top: 2px solid rgba(0, 0, 0, 0.2); padding-top: 30px" >
-                        <li><strong> Fácil de usar: </strong> Plataforma intuitiva e amigável, ideal para usuários de todos os níveis.</li>
-                        <li><strong> Completa: </strong> Oferecemos todos os recursos que você precisa para gerenciar suas notas fiscais,
-                            desde
-                            a criação até a entrega.</li>
-                        <li><strong> Confiável:</strong> Somos uma empresa certificada pela SEFAZ, garantindo a segurança e confiabilidade
-                            de
-                            seus dados fiscais.</li>
-                        <li><strong> Suporte especializado:</strong> Contamos com uma equipe de especialistas em NFe e NFC-e à disposição
-                            para
-                            te ajudar.</li>
-                    </ul>
-			
-                   <strong> <p>
-                        A SpeedNFe é a solução ideal para empresas de todos os portes que desejam:
-                    </p></strong>
-					
-                    <ul >
-                        <li>Agilizar a emissão de notas fiscais;</li>
-                        <li>Reduzir custos;</li>
-                        <li>Aumentar a produtividade;</li>
-                        <li>Ter mais segurança e confiabilidade em seus processos fiscais.</li>
-                    </ul>
-				</strong>
-                </header>
-			</section>
+
+                        <h3 style="margin-top: 25px">Nossos Diferenciais</h3>
+
+                        <ul style="border-top: 2px solid rgba(0, 0, 0, 0.2); padding-top: 30px">
+                            <li><strong> Fácil de usar: </strong> Plataforma intuitiva e amigável, ideal para usuários de todos os níveis.</li>
+                            <li><strong> Completa: </strong> Oferecemos todos os recursos que você precisa para gerenciar suas notas fiscais,
+                                desde
+                                a criação até a entrega.</li>
+                            <li><strong> Confiável:</strong> Somos uma empresa certificada pela SEFAZ, garantindo a segurança e confiabilidade
+                                de
+                                seus dados fiscais.</li>
+                            <li><strong> Suporte especializado:</strong> Contamos com uma equipe de especialistas em NFe e NFC-e à disposição
+                                para
+                                te ajudar.</li>
+                        </ul>
+
+                        <strong>
+                            <p>
+                                A SpeedNFe é a solução ideal para empresas de todos os portes que desejam:
+                            </p>
+                        </strong>
+
+                        <ul>
+                            <li>Agilizar a emissão de notas fiscais;</li>
+                            <li>Reduzir custos;</li>
+                            <li>Aumentar a produtividade;</li>
+                            <li>Ter mais segurança e confiabilidade em seus processos fiscais.</li>
+                        </ul>
+                        </strong>
+                    </header>
+                </section>
             </section>
-       
 
 
-        <!-- CTA -->
-        <section id="cta">
 
-            <h2>Solicite já uma demonstração</h2>
-            <p>Experimente uma Demonstração Gratuita do Nosso Sistema</p>
+            <!-- CTA -->
+            <section id="cta">
 
-            <form>
-                <div class="row gtr-50 gtr-uniform">
-                    <div class="col-8 col-12-mobilep">
-                        <input type="email" name="email" id="email" placeholder="Email" />
+                <h2>Solicite já uma demonstração</h2>
+                <p>Experimente uma Demonstração Gratuita do Nosso Sistema</p>
+
+                <form>
+                    <div class="row gtr-50 gtr-uniform">
+                        <div class="col-8 col-12-mobilep">
+                            <input type="email" name="email" id="email" placeholder="Email" />
+                        </div>
+                        <div class="col-4 col-12-mobilep">
+                            <input type="submit" value="Solicitar" class="fit" />
+                        </div>
                     </div>
-                    <div class="col-4 col-12-mobilep">
-                        <input type="submit" value="Solicitar" class="fit" />
-                    </div>
-                </div>
-            </form>
+                </form>
 
-        </section>
+            </section>
 
-        <!-- Footer -->
-        <footer id="footer">
-            <ul class="icons">
-                <li><a href="https://f-softsistemas.com.br/" target="_blank" class="icon brands fa-facebook-f"><span
-                            class="label">Facebook</span></a></li>
-                <li><a href="https://www.instagram.com/fsoft_sistemas/" target="_blank"
-                        class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-                <li><a href="https://github.com/fsoftsistemas" target="_blank" class="icon brands fa-github"><span
-                            class="label">Github</span></a></li>
-                <li><a href="https://g.co/kgs/CjviXRU" target="_blank" class="icon brands fa-google-plus"><span
-                            class="label">Google+</span></a></li>
-            </ul>
-            <ul class="copyright">
-                <li>&copy; FSOFT SISTEMAS. All rights reserved.</li>
-                <li>Design: <a href="https://f-softsistemas.com.br/">FSOFT SISTEMAS</a></li>
-            </ul>
-        </footer>
+            <!-- Footer -->
+            <footer id="footer">
+                <ul class="icons">
+                    <li><a href="https://f-softsistemas.com.br/" target="_blank" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+                    <li><a href="https://www.instagram.com/fsoft_sistemas/" target="_blank" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+                    <li><a href="https://github.com/fsoftsistemas" target="_blank" class="icon brands fa-github"><span class="label">Github</span></a></li>
+                    <li><a href="https://g.co/kgs/CjviXRU" target="_blank" class="icon brands fa-google-plus"><span class="label">Google+</span></a></li>
+                </ul>
+                <ul class="copyright">
+                    <li>&copy; FSOFT SISTEMAS. All rights reserved.</li>
+                    <li>Design: <a href="https://f-softsistemas.com.br/">FSOFT SISTEMAS</a></li>
+                </ul>
+            </footer>
 
-    </div>
+        </div>
 
-    <!-- Scripts -->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/jquery.dropotron.min.js"></script>
-    <script src="assets/js/jquery.scrollex.min.js"></script>
-    <script src="assets/js/browser.min.js"></script>
-    <script src="assets/js/breakpoints.min.js"></script>
-    <script src="assets/js/util.js"></script>
-    <script src="assets/js/main.js"></script>
+        <!-- Scripts -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/jquery.dropotron.min.js"></script>
+        <script src="assets/js/jquery.scrollex.min.js"></script>
+        <script src="assets/js/browser.min.js"></script>
+        <script src="assets/js/breakpoints.min.js"></script>
+        <script src="assets/js/util.js"></script>
+        <script src="assets/js/main.js"></script>
 
 </body>
 
