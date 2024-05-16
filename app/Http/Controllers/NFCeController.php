087001c848aaf4e7c914908c2fc4dda73b2a2281
FSOFTSistemas <fsoftsistemas@gmail.com>
2024-05-16 09:27:56 -0300

    feat: Criar controladores para entidades relacionadas a NFCe

---
+++ app/Http/Controllers/NFCeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NFCeController extends Controller
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+}
