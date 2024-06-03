b712f30cda2ab8758915fd24adade88f5c6c458e
fsoft-development <fsoftsistemas@gmail.com>
2024-06-03 10:00:35 -0300

    feat(dev): Criar método para calcular altura do cupom de acordo com a quantidade de itens de uma venda
    feat(dev): Criar método para calcular altura do cupom de acordo com a quantidade de itens de uma venda

--- app/Utils/CalculateCouponHeight.php
+++ app/Utils/CalculateCouponHeight.php
@@ -4,5 +4,15 @@ namespace App\Utils;
 
 class CalculateCouponHeight
 {
+    public static function calculate($itemsLength)
+    {
+        $height = 150;
+        if ($itemsLength > 1) {
+            for ($i = 1; $i < $itemsLength; $i++) {
+                $height += 15;
+            }
+        }
+        return $height;
+    }
 
 }
