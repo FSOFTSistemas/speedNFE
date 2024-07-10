84fdd565ce60705b36c4ef1a4bc36c7409c420d7
fsoft-development <fsoftsistemas@gmail.com>
2024-07-10 14:53:03 -0300

    chore(AT-81): Ajustar cálculo para reverter estoque

--- app/Services/EstoquesService.php
+++ app/Services/EstoquesService.php
@@ -32,8 +32,8 @@ class EstoquesService{
     {
         $stock = Estoque::whereProdutoId($prodId)->first();
         return $stock->update([
-            'estoque_atual' => $stock->estoque_anterior,
-            'estoque_anterior' => $stock->estoque_anterior == $stock->entradas ? 0 : $stock->estoque_atual + $amount,
+            'estoque_atual' => $stock->estoque_atual + $amount,
+            'estoque_anterior' => $stock->estoque_anterior == $stock->entradas ? 0 : $stock->estoque_anterior + $amount,
             'saidas' => $stock->saidas - $amount
         ]);
     }
