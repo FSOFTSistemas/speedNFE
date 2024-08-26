906f2930f91e491ca65dfd1caca5a4aa249c33d1
Marco Moraes <marcoantoniomoraes760@gmail.com>
2024-08-26 14:45:24 -0300

    feat: Criar tela para exibição do histórico de pagamentos da assinatura

--- resources/views/faturas/payment-history.blade.php
+++ resources/views/faturas/payment-history.blade.php
@@ -1,13 +1,69 @@
 @extends('adminlte::page')
 
-@section('title', 'Assinatura')
+@section('title', 'Histórico de pagamentos')
 
 @section('content_header')
     <div class="text-center text-dark">
-        <h3>Assinatura</h3>
+        <h3>Histórico de pagamentos</h3>
     </div>
 @stop
 
 @section('content')
-    <h1>Bro3, Are you gay?</h1>
+    <section>
+        <div class="card">
+            <div class="card-content">
+                <div class="card-body">
+                    @component('components.dataTable', [
+                        'responsive' => [
+                            [
+                                'responsivePriority' => 1,
+                                'targets' => 0,
+                            ],
+                            [
+                                'responsivePriority' => 2,
+                                'targets' => 1,
+                            ],
+                            [
+                                'responsivePriority' => 3,
+                                'targets' => 2,
+                            ],
+                            [
+                                'responsivePriority' => 4,
+                                'targets' => 3,
+                            ],
+                            [
+                                'responsivePriority' => 5,
+                                'targets' => -1,
+                            ],
+                        ],
+                        'searching' => true,
+                        'lengthChange' => true,
+                        'pageLength' => 50,
+                        'ordering' => true,
+                        'showFooter' => false,
+                    ])
+                        <thead class="table-primary">
+                            <tr>
+                                <th>ID do pagamento</th>
+                                <th>ID da fatura</th>
+                                <th>Serviço</th>
+                                <th>Pago em</th>
+                                <th>Valor</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>534534</td>
+                                <td>2543524</td>
+                                <td>Parcela do speed</td>
+                                <td>2024-08-14</td>
+                                <td>R$64.99</td>
+                            </tr>
+                        </tbody>
+                    @endcomponent
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/faturas/signature.blade.php
+++ resources/views/faturas/signature.blade.php
@@ -13,63 +13,73 @@
         <div class="card">
             <div class="card-content">
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col">
-                            <h6>
-                                <b>Licença para plataforma - Speed NFe</b>
-                            </h6>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col">
-                            <h6>Mensal</h6>
-                        </div>
-                    </div>
-
-                    <div class="row pt-3">
-                        <div class="col">
-                            <h6><i class="fas fa-history text-teal"></i> Renovação automática</h6>
-                            <div class="row">
-                                <div class="col">
-                                    <h6>ativada</h6>
-                                </div>
+                    @if (isset($signature->due))
+                        <div class="row">
+                            <div class="col">
+                                <h6>
+                                    <b>Licença para plataforma - Speed NFe</b>
+                                </h6>
                             </div>
                         </div>
 
-                        <div class="col">
-                            <h6>Data de expiração</h6>
-                            <div class="row">
-                                <div class="col">
-                                    <h6 title="{{ isset($signature->due->expires_in) ? 'Expira em breve' : 'Expirado' }}"><i
-                                            class="fas fa-exclamation-circle text-pink"></i>
-                                        {{ $signature->due->expired_on ?? $signature->due->expires_in }}</h6>
-                                </div>
+                        <div class="row">
+                            <div class="col">
+                                <h6>Mensal</h6>
                             </div>
                         </div>
 
-                        <div class="col">
-                            <h6>Preço de renovação</h6>
-                            <div class="row">
-                                <div class="col">
-                                    <h6>R${{ number_format($signature->value, 2, '.', '.') }}</h6>
+                        <div class="row pt-3">
+                            <div class="col">
+                                <h6><i class="fas fa-history text-teal"></i> Renovação automática</h6>
+                                <div class="row">
+                                    <div class="col">
+                                        <h6>ativada</h6>
+                                    </div>
                                 </div>
+                            </div>
+
+                            <div class="col">
+                                <h6>Data de expiração</h6>
+                                <div class="row">
+                                    <div class="col">
+                                        <h6
+                                            title="{{ isset($signature->due->expires_in) ? 'Expira em breve' : 'Expirado' }}">
+                                            <i class="fas fa-exclamation-circle text-pink"></i>
+                                            {{ $signature->due->expired_on ?? $signature->due->expires_in }}
+                                        </h6>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col">
+                                <h6>Preço de renovação</h6>
+                                <div class="row">
+                                    <div class="col">
+                                        <h6>R${{ number_format($signature->value, 2, '.', '.') }}</h6>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col">
+                                <a class="btn btn-outline-primary border-secondary" data-toggle="modal"
+                                    data-target="#renewSignatureModal">Renovar agora</a>
                             </div>
                         </div>
 
-                        <div class="col">
-                            <a class="btn btn-outline-primary border-secondary" data-toggle="modal" data-target="#renewSignatureModal">Renovar agora</a>
+                        @component('components.modal', [
+                            'modalId' => 'renewSignatureModal',
+                            'modalTitle' => 'Renovar Assinatura',
+                            'sizeModal' => 'modal-lg',
+                        ])
+                        @endcomponent
+                    @else
+                        <div class="text-center">
+                            <h5>Seu perfil é isento de assinaturas dentro de nossa plataforma! <br> Aproveite gratuitamente
+                                as nossas funcionalidades</h5>
                         </div>
-                    </div>
+                    @endif
                 </div>
             </div>
         </div>
     </section>
-
-    @component('components.modal', [
-        'modalId' => 'renewSignatureModal',
-        'modalTitle' => 'Renovar Assinatura',
-        'sizeModal' => 'modal-lg',
-    ])
-    @endcomponent
 @endsection
